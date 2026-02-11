0f7bb182372c4845d72597af454890aa924f1665
yangjingjing10 <yjj1345391577@gmail.com>
2026-02-11 13:06:46 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+// Service Worker
+const CACHE_NAME = 'myphone-v1';
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        './index.html',
+        './'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
